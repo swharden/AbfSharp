28526f3551576ea2aec4882f1ffda8c7744656b0
Scott W Harden <swharden@gmail.com>
2020-04-21 10:57:07 -0400

    test 3d plot

--- src/ABFsharp.Tests/Plot.cs
+++ src/ABFsharp.Tests/Plot.cs
@@ -29,7 +29,7 @@ namespace ABFsharp.Tests
             if (tightX)
                 plt.AxisAuto(0);
 
-            plt.Title(title);
+            plt.Title(title.Trim());
             plt.YLabel(yLabel);
             plt.XLabel(xLabel);
             plt.Legend(location: ScottPlot.legendLocation.upperRight);
@@ -113,6 +113,54 @@ namespace ABFsharp.Tests
             plt.Grid(false);
             plt.Ticks(displayTicksY: false);
             SaveFig(plt, "Stacked Sweeps", xLabel: "Sweep Time (seconds)", yLabel: null);
+        }
+
+        Color Mix(Color colorA, Color colorB, double fracA)
+        {
+            byte r = (byte)((colorA.R * (1 - fracA)) + colorB.R * fracA);
+            byte g = (byte)((colorA.G * (1 - fracA)) + colorB.G * fracA);
+            byte b = (byte)((colorA.B * (1 - fracA)) + colorB.B * fracA);
+            return Color.FromArgb(r, g, b);
+        }
+
+        Color Mix(string hexA, string hexB, double fracA)
+        {
+            Color colorA = ColorTranslator.FromHtml(hexA);
+            Color colorB = ColorTranslator.FromHtml(hexB);
+            return Mix(colorA, colorB, fracA);
+        }
+
+        [Test]
+        public void Test_Plot_3D()
+        {
+            (ABF abf, ScottPlot.Plot plt) = GetAbfAndPlot(sampleABFs.CurrentClampSteps);
+
+            for (int i = abf.sweepCount - 1; i >= 0; i--)
+            {
+                double frac = (double)i / (abf.sweepCount - 1);
+                Color color = Mix("#8181f2", "#a2f0ca", frac);
+                var trace = abf.GetSweep(i);
+                plt.PlotSignal(
+                    ys: trace.values,
+                    sampleRate: trace.sampleRate,
+                    color: color,
+                    yOffset: i * 15,
+                    xOffset: i * .025, 
+                    maxRenderIndex: 20000, lineWidth: 2);
+            }
+
+            // hack-in an L-shaped scalebar until ScottPlot supports it natively
+            plt.PlotLine(1.15, -50, 1.25, -50, lineWidth: 2, color: Color.Black);
+            plt.PlotLine(1.25, -50, 1.25, -10, lineWidth: 2, color: Color.Black);
+            plt.PlotText("100 ms", 1.2, -50, color: Color.Black, fontSize: 16,
+                alignment: ScottPlot.TextAlignment.upperCenter);
+            plt.PlotText("40 mV", 1.25, -58, color: Color.Black, fontSize: 16, rotation: -90);
+
+            plt.Grid(false);
+            plt.Ticks(false, false);
+            plt.Frame(false);
+            plt.Resize(800, 600);
+            SaveFig(plt, " ", null, null);
         }
     }
 }
