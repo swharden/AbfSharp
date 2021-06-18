8f5411dec437bc663605f72ae31bffe6cc587551
Scott W Harden <swharden@gmail.com>
2021-06-17 23:05:21 -0400

    ABF2: refactor section management

--- src/AbfSharp/RawReaderABF2.cs
+++ src/AbfSharp/RawReaderABF2.cs
@@ -12,37 +12,29 @@ namespace AbfSharp
     /// </summary>
     public class RawReaderABF2 : RawReader
     {
-        UInt32 protocolSection_firstByte;
-        UInt32 protocolSection_size;
-        UInt32 protocolSection_count;
-        UInt32 adcSection_firstByte;
-        UInt32 adcSection_size;
-        UInt32 adcSection_count;
-        UInt32 stringsSection_firstByte;
-        UInt32 stringsSection_size;
-        UInt32 stringsSection_count;
-        UInt32 dataSection_firstByte;
-        UInt32 dataSection_size;
-        UInt32 dataSection_count;
+        private struct Section
+        {
+            public readonly long FirstByte;
+            public readonly long Size;
+            public readonly long Count;
+            public Section(long fb, long sz, long ct) => (FirstByte, Size, Count) = (fb, sz, ct);
+        }
+
+        private readonly Section ProtocolSection;
+        private readonly Section AdcSection;
+        private readonly Section StringsSection;
+        private readonly Section DataSection;
 
         public RawReaderABF2(BinaryReader reader) : base(reader)
         {
             // ABF2 files have different sections that start at different byte locations.
             // The location of each section is stored at fixed byte positions at the start of the file.
+            Section GetSection(long position) => new(ReadUInt32(position) * 512, ReadUInt32(), ReadUInt32());
 
-            // get section byte location information
-            protocolSection_firstByte = ReadUInt32(76) * 512;
-            protocolSection_size = ReadUInt32();
-            protocolSection_count = ReadUInt32();
-            adcSection_firstByte = ReadUInt32(92) * 512;
-            adcSection_size = ReadUInt32();
-            adcSection_count = ReadUInt32();
-            stringsSection_firstByte = ReadUInt32(220) * 512;
-            stringsSection_size = ReadUInt32();
-            stringsSection_count = ReadUInt32();
-            dataSection_firstByte = ReadUInt32(236) * 512;
-            dataSection_size = ReadUInt32();
-            dataSection_count = ReadUInt32();
+            ProtocolSection = GetSection(76);
+            AdcSection = GetSection(92);
+            StringsSection = GetSection(220);
+            DataSection = GetSection(236);
         }
 
         public override Version GetFileVersion()
@@ -54,7 +46,7 @@ namespace AbfSharp
         {
             // operation mode is gap free, episodic, etc.
             // in ABF2 files it's an integer at the start of the protocol section.
-            int nOperationMode = ReadUInt16(protocolSection_firstByte);
+            int nOperationMode = ReadUInt16(ProtocolSection.FirstByte);
             return (OperationMode)nOperationMode;
         }
     }
