a97be74bff7007348608f984f7577b871ab94430
Scott W Harden <swharden@gmail.com>
2021-06-17 21:52:43 -0400

    RawAbf: create a native C# ABF file reader

---
+++ src/AbfSharp/RawABF.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AbfSharp
+{
+    /// <summary>
+    /// This ABF reader uses native .NET code (not the ABFFIO.DLL) so it can be used on other platforms
+    /// like 64-bit Windows, Linux, MacOS, etc. It is more performant than ABFFIO.DLL for many operations,
+    /// but not all features are supported.
+    /// </summary>
+    public class RawABF
+    {
+        public readonly string Path;
+
+        public readonly Version AbfVersion;
+
+        public RawABF(string abfFilePath)
+        {
+            // validate file path
+            if (!File.Exists(abfFilePath))
+                throw new FileNotFoundException($"file does not exist: {abfFilePath}");
+            Path = System.IO.Path.GetFullPath(abfFilePath);
+
+            // open the file and locally maintain the reader
+            using BinaryReader reader = new(File.Open(Path, FileMode.Open));
+
+            // read the first few bytes of the file to confirm it's an ABF
+            byte[] signatureBytes = reader.ReadBytes(4);
+            string signature = Encoding.ASCII.GetString(signatureBytes);
+            if (!signature.StartsWith("ABF"))
+                throw new FileLoadException($"file does not have expected ABF signature: {Path}");
+
+            // character 4 indicaites if its an ABF1 or ABF2 format.
+            // In ABF1 files I've seen byte 4 as 20 and also 50
+            if (signature == "ABF ")
+                AbfVersion = new Version(1, 0);
+            else if (signature == "ABF2")
+                AbfVersion = new Version(2, 0);
+            else
+                throw new FileLoadException($"unsupported ABF version (signature: {BitConverter.ToString(signatureBytes)}");
+
+            // close the file
+            reader.Close();
+        }
+    }
+}
---
+++ src/AbfSharpTests/RawAbf.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbfSharpTests
+{
+    class RawAbf
+    {
+        [Test]
+        public void Test_MatchesOfficial_FileMajorVersion()
+        {
+            foreach (string abfFilePath in SampleData.GetAllAbfPaths())
+            {
+                var official = new AbfSharp.ABF(abfFilePath);
+                var raw = new AbfSharp.RawABF(abfFilePath);
+
+                if (official.Header.HeaderStruct.fFileVersionNumber < 2)
+                    Assert.AreEqual(1, raw.AbfVersion.Major);
+                else
+                    Assert.AreEqual(2, raw.AbfVersion.Major);
+            }
+        }
+    }
+}
