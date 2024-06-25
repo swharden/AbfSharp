e4e26cb69d3c0765920eee27adc5cb546d6bf876
Scott W Harden <swharden@gmail.com>
2024-06-25 18:42:56 -0400

    Tests: improve how sample data files are located

--- src/AbfSharpTests/SampleData.cs
+++ src/AbfSharpTests/SampleData.cs
@@ -2,8 +2,35 @@ namespace AbfSharpTests;
 
 public static class SampleData
 {
-    public static readonly string DATA_FOLDER = Path.Combine(TestContext.CurrentContext.TestDirectory, "../../../../../dev/abfs/");
-    public static readonly string GRAPHICS_FOLDER = Path.Combine(TestContext.CurrentContext.TestDirectory, "../../../../../dev/graphics/");
+    public static readonly string REPO_ROOT = GetRepoRoot();
+
+    private static string GetRepoRoot()
+    {
+        string folder = Path.GetFullPath("./");
+
+        for (int i = 0; i < 100; i++)
+        {
+            string parentFolder = Path.GetDirectoryName(folder);
+            string licenseFile = Path.Combine(parentFolder, "LICENSE");
+            if (File.Exists(licenseFile))
+                return parentFolder;
+            folder = parentFolder;
+        }
+
+        throw new InvalidOperationException("license file never found");
+    }
+
+    public static readonly string DATA_FOLDER = Path.Combine(REPO_ROOT, "dev/abfs/");
+    public static readonly string GRAPHICS_FOLDER = Path.Combine(REPO_ROOT, "dev/graphics/");
+
+    [Test]
+    public static void Test_Path_RepoRoot() => Directory.Exists(REPO_ROOT).Should().BeTrue();
+
+    [Test]
+    public static void Test_Path_DataFolder() => Directory.Exists(DATA_FOLDER).Should().BeTrue();
+
+    [Test]
+    public static void Test_Path_GraphicsFolder() => Directory.Exists(GRAPHICS_FOLDER).Should().BeTrue();
 
     public static string[] GetAllAbfPaths() => Directory.GetFiles(DATA_FOLDER, "*.abf");
 
