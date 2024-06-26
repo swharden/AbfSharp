9bd9b33393a439caed51e29f38d45ba205692244
Scott W Harden <swharden@gmail.com>
2024-06-26 07:30:00 -0400

    Version: provide access to assembly version in code

---
+++ src/AbfSharp/Version.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace AbfSharp;
+
+public static class Version
+{
+    /// <summary>
+    /// Version formatted like "1.2.3-beta"
+    /// </summary>
+    public static string VersionString { get; private set; } = GetVersionString();
+
+    private static string GetVersionString()
+    {
+        string v = Assembly.GetAssembly(typeof(ABF))!
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
+            .InformationalVersion;
+
+        return v.Contains('+') ? v.Split('+')[0] : v;
+    }
+
+}
